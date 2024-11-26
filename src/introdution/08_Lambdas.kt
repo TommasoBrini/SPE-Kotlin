31ffcecd06bc76a0e70685f7ee24d8e599929ad5
Tommaso <tommaso.brini@studio.unibo.it>
2024-11-26 16:59:22 +0100

    solved ex08

---
+++ src/introdution/08_Lambdas.kt
@@ -0,0 +1,8 @@
+package introdution
+
+fun containsEven(collection: Collection<Int>): Boolean =
+    collection.any { x -> (x % 2 == 0) }
+
+fun main(){
+    println(containsEven(listOf(3,7,5,7)))
+}
