39945a261bd8effee53fc019b0ac782def941d49
Tommaso <tommaso.brini@studio.unibo.it>
2024-11-27 23:41:03 +0100

    Solved collections ex8

---
+++ src/04_collections/08_flatMap.kt
@@ -0,0 +1,9 @@
+package `04_collections`
+
+// Return all products the given customer has ordered
+fun Customer.getOrderedProducts(): List<Product> =
+    orders.flatMap{it.products}
+
+// Return all products that were ordered by at least one customer
+fun Shop.getOrderedProducts(): Set<Product> =
+    customers.flatMap{it.getOrderedProducts()}.toSet()
