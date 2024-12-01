fa7efb8db53488b5d3224ddae06b45faaa90dfd0
Tommaso <tommaso.brini@studio.unibo.it>
2024-12-01 01:41:59 +0100

    solved exercise of html internal dsl

--- src/02-DSL/HTML_DSL.kt
+++ src/02-DSL/HTML_DSL.kt
@@ -1,6 +1,9 @@
 const val INDENT = "\t"
 typealias Attribute = Pair<String, String> // Just to avoid writing Pair<String, String> every time
 
+@DslMarker
+annotation class HtmlTagMarker
+
 // Element interface
 interface Element {
     fun render(indent: String = ""): String
@@ -61,6 +64,7 @@ abstract class AbstractTag(override val name: String, vararg attributes: Attribu
 }
 
 // Entry point of the DSL
+@HtmlTagMarker
 class HTML(vararg attributes: Attribute = arrayOf()): AbstractTag("html", *attributes){
     fun head(configuration: Head.() -> Unit) = registerElement(Head().apply(configuration))
     fun body(vararg attributes: Attribute, configuration: Body.() -> Unit) = registerElement(Body(*attributes).apply(configuration))
@@ -68,18 +72,38 @@ class HTML(vararg attributes: Attribute = arrayOf()): AbstractTag("html", *attri
 
 fun html(vararg attributes: Attribute, init: HTML.() -> Unit): HTML = HTML(*attributes).apply(init)
 
-// Implementation of the Tag interface => Head tag!
+// Implementation of the Tag interface => Head tag
+@HtmlTagMarker
 class Head(): AbstractTag("head"){
     fun title(configuration: Title.() -> Unit) = registerElement(Title().apply(configuration))
 }
 
+@HtmlTagMarker
 abstract class TagWithText(name: String, vararg attributes: Attribute): AbstractTag(name, *attributes) {
     operator fun String.unaryMinus() = registerElement(Text(this))
 }
 
 class Title: TagWithText("title")
 
-class Body(vararg attributes: Attribute): TagWithText("body", *attributes)
+//class Body(vararg attributes: Attribute): TagWithText("body", *attributes)
+
+abstract class BodyTag(name: String, vararg attributes: Attribute): TagWithText(name, *attributes) {
+    // <a> and <p> can be nested everywhere in the body
+    fun p(vararg attributes: Attribute, configuration: P.() -> Unit) =
+        registerElement(P(*attributes).apply(configuration))
+    fun a(href: String, vararg attributes: Attribute, configuration: Anchor.() -> Unit) =
+        registerElement(Anchor(href, *attributes).apply(configuration))
+}
+
+class Body(vararg attributes: Attribute): BodyTag("body", *attributes)
+
+class P(vararg attributes: Attribute): BodyTag("p", *attributes), RepeatableElement
+
+class Anchor(
+    href: String? = null,
+    vararg attributes: Attribute
+):  BodyTag("a", *(if (href == null) emptyArray() else arrayOf("href" to href)) + attributes),
+    RepeatableElement
 
 const val newline = "<br/>"
 
@@ -87,11 +111,14 @@ fun main() {
     println(html("lang" to "en") {
         head { title { -"A link to the unibo webpage" }}
         body {
-            -"My contents"
-            -newline
-            -"And some more contents"
+            p("class" to "myCustomCssClass"){
+                a(href = "https://www.unibo.it") {
+                    -"Unibo Website"
+                }
+            }
         }
     }.render())
+
 }
 
 
